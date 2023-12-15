4b7731bd09c30a22676465a4c7fde1201181d3d5
yosefbenti <yosefbenti61@gmail.com>
2023-12-15 11:28:45 +0300

    new file added

---
+++ login.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+       const bgImage = document.querySelector("#background");
+       const newImageURL = 'file:///F:/Programming/repos/images/CofeeLogin.jpg';
+       bgImage.style.backgroundImage = `url(${newImageURL})`;
+
+});
